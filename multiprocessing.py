e870b6e07971d030c8d1ef54eadd61314637aa6c
Regina0101 <ritodo1010@gmail.com>
2024-05-16 14:07:01 +0100

    added logging

--- multiprocessing.py
+++ multiprocessing.py
@@ -1,17 +1,24 @@
+import logging
 from multiprocessing import Pool, cpu_count
 
 def factorize_number(number):
+    logging.info(f'Starting factorization for {number}')
     factors = []
     for i in range(1, number + 1):
         if number % i == 0:
             factors.append(i)
+    logging.info(f'Finished factorization for {number}')
     return factors
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(processName)s - %(message)s')
+
     numbers = [128, 255, 99999, 10651060]
 
+    logging.info('Starting parallel factorization')
     with Pool(processes=cpu_count()) as pool:
         results = pool.map(factorize_number, numbers)
+    logging.info('Finished parallel factorization')
 
     a, b, c, d = results
 
@@ -20,4 +27,4 @@ if __name__ == "__main__":
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
     assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
-    print("All tests passed.")
+    logging.info('All tests passed.')
--- threads.py
+++ threads.py
@@ -1,3 +1,4 @@
+import logging
 from threading import Thread
 from pathlib import Path
 from shutil import copyfile
@@ -7,18 +8,28 @@ def copy_file(file):
     new_path = Path("dist") / suf
     new_path.mkdir(parents=True, exist_ok=True)
     copyfile(file, new_path / file.name)
+    logging.info(f'Copied file {file} to {new_path / file.name}')
 
 def file_copy_task(file):
+    logging.info(f'Starting to copy file {file}')
     copy_file(file)
+    logging.info(f'Finished copying file {file}')
 
 def file_finder(path):
     for file in path.iterdir():
         if file.is_dir():
+            logging.info(f'Entering directory {file}')
             file_finder(file)
         else:
+            logging.info(f'Found file {file}, starting copy in a new thread')
             thread = Thread(target=file_copy_task, args=(file,))
             thread.start()
             thread.join()
 
-path = Path('hlam')
-file_finder(path)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(threadName)s - %(message)s')
+
+    path = Path('hlam')
+    logging.info(f'Starting to scan directory {path}')
+    file_finder(path)
+    logging.info('Finished scanning and copying files')
